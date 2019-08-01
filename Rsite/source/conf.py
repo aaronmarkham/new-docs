c54d597c746c54480234f4a8a6109d996e3d9199
Aaron Markham <markhama@amazon.com>
2019-08-01 16:07:52 -0700

    move theme into project ;

--- Rsite/source/conf.py
+++ Rsite/source/conf.py
@@ -154,7 +154,7 @@ suppress_warnings = [
 # -- Options for HTML output ---------------------------------------------------
 
 # Add any paths that contain custom themes here, relative to this directory.
-html_theme_path = ['mxtheme']
+html_theme_path = ['../../mxtheme']
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
